265d9d42b849f265249a2cfe01c9de13a69216be
spiderman <ak188269@gmail.com>
2023-05-07 23:54:11 +0530

    cors error is happening

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const app=express();
 dotenv.config({path:"./config.env"});
 app.use(cors({origin:"*",credentials:true}));
 const PORT=process.env.PORT || 8000;
-startDb();
+// startDb();
 app.route("/").get((req,res)=>{
     res.send("welcome to node app");
 })
 app.get("/get",async(req,res)=>{
   try {
-     console.log("get is called");
+    //  console.log("get is called");
     const data={
         name:"Avinash",
         email:"ak122@gmail.com",
         password:"password"
     }
-   const user= await User.create({
-   ...data
-});
-    res.json({user})
+//    const user= await User.create({
+//    ...data
+// });
+//     res.json({user})
+res.send(data);
 
 }
     
 catch(err) {
-    console.log(err.message);
     res.json({success: false, error: err.message});
 }
 }
