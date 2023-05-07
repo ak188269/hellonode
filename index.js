9a3ab012340681f5e01439aa0b256fa4b366f1b2
spiderman <ak188269@gmail.com>
2023-05-07 23:28:00 +0530

    added mongoose database

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 import  express  from "express";
 import dotenv from 'dotenv'
 import cors from "cors";
-
+import startDb from "./database.js";
+// const User =require("./user");
+import User from "./user.js";
 const app=express();
 dotenv.config({path:"./config.env"});
 app.use(cors({origin:"*",credentials:true}));
 const PORT=process.env.PORT || 8000;
+startDb();
 app.route("/").get((req,res)=>{
     res.send("welcome to node app");
 })
-app.get("/get", (req,res)=>{
-    res.json({
-        Name:"Avinash",
-        Email:"ak122@gmail.com",
-        Password:"password"
-    })
+app.get("/get",async(req,res)=>{
+  try {
+     console.log("get is called");
+    const data={
+        name:"Avinash",
+        email:"ak122@gmail.com",
+        password:"password"
+    }
+   const user= await User.create({
+   ...data
+});
+    res.json({user})
+
+}
+    
+catch(err) {
+    console.log(err.message);
+    res.json({success: false, error: err.message});
+}
 }
     )
 app.listen(PORT,()=>{
