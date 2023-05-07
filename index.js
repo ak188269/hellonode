a9e9b226df6fb787946e81cc88cb292e5d337564
spiderman <ak188269@gmail.com>
2023-05-07 21:26:49 +0530

    removerd mistake in index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import  express  from "express";
 import dotenv from 'dotenv'
-const bodyParser=require("body-parser");
-app.use(bodyParser.json({limit:"10mb"}));
-// app.use(express.json({limit:"10mb"}));
-// app.use(express.urlencoded({extended:true,limit:"10mb"}));
+// const bodyParser=require("body-parser");
+// app.use(bodyParser.json({limit:"10mb"}));
+
 const cors=require("cors");
-app.use(bodyParser.urlencoded({extended:true,limit:"10mb"}));
+// app.use(bodyParser.urlencoded({extended:true,limit:"10mb"}));
 app.use(cors({origin:["http://localhost:3000"],credentials:true}));
 const app=express();
 dotenv.config({path:"./config.env"});
