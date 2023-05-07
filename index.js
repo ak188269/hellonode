ccedad2d0e5303dde2b6d9e7d51a654c2a0140f8
spiderman <ak188269@gmail.com>
2023-05-08 02:15:32 +0530

    version 31

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import startDb from "./database.js";
 import User from "./user.js";
 const app=express();
 dotenv.config({path:"./config.env"});
-app.use(cors({origin:["http://localhost:3000/"],credentials:true}));
+app.use(cors({origin:"*",credentials:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 const PORT=process.env.PORT || 8000;
