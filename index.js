752b93b42b2c65511d22e1af20035549be46f36f
spiderman <ak188269@gmail.com>
2023-05-07 20:38:56 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import  express  from "express";
 import dotenv from 'dotenv'
+const bodyParser=require("body-parser");
+app.use(bodyParser.json({limit:"10mb"}));
+// app.use(express.json({limit:"10mb"}));
+// app.use(express.urlencoded({extended:true,limit:"10mb"}));
+const cors=require("cors");
+const auth = require("./middleware/Auth");
+app.use(bodyParser.urlencoded({extended:true,limit:"10mb"}));
+app.use(cors({origin:["http://localhost:3000"],credentials:true}));
 const app=express();
 dotenv.config({path:"./config.env"});
 const PORT=process.env.PORT || 8000;
