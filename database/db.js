415cf577163b6bf46b0bd8594bc87d953db4bdd0
Avinash <84312627+ak188269@users.noreply.github.com>
2023-05-08 12:30:17 +0530

    version 36

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose");
 // const uri="mongodb://127.0.0.1:27017/SocialSite";
 const uri=process.env.DATABASE_URL;
+
 const connectToDatabase= async ()=>{
-await mongoose.connect(process.env.DATABASE_URL);
+await mongoose.connect(`${uri}`);
     // console.log("connected to database");
 }
 module.exports=connectToDatabase;
