9973159c91031651d91b88e218a56777f5493bdc
spiderman <ak188269@gmail.com>
2023-05-08 12:10:14 +0530

    version 35

--- middleware/isActive.js
+++ middleware/isActive.js
@@ -3,7 +3,7 @@ const User = require("../model/User")
 const isActive =async(req,res,next) =>{
 let user=await User.findById(req.user.id);
 if(user.isActive===false){
-    res.json({success:false,msg:"Please login first"})
+    res.json({success:false,msg:"You are not logged in"})
 }
 else return next();
 }
